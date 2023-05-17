ea4be9254a109bd37c4c348a60f4ada9d418022a
L0elements <leoelements08@gmail.com>
2023-05-17 21:09:55 +0200

    Feature: Add a new book On branch Args_read_feature new file:   Data/bookone modified:   main.py modified:   venv/Lib/site-packages/local/Lockpass.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-import local.Lockpass
+import sys
+import pathlib
+from local.Lockpass import lockpass
 class start():
     def error():
         return 0
     def SystemControl():
+        path = pathlib.PureWindowsPath("./main.py")
+        print(path)
         return 0
 
     def Argvcheck():
-        print("Hello World")
-
+        try:
+            command = sys.argv[1]
+        except IndexError:
+            lockpass.help()
+            return 2 #noarguments
+        print(sys.argv)
+        if command == 'newbook':
+            lockpass.newbook("bookone") # type: ignore
 if not start.SystemControl():
     start.Argvcheck()
 else:
