0020e7862dcdce85d37c02908b71491fd079f04d
L0elements <leoelements08@gmail.com>
2023-05-17 19:17:54 +0200

    Initial commit Changes committed: new file:   main.py new file:   venv/Lib/site-packages/local/Lockpass.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import local.Lockpass
+class start():
+    def error():
+        return 0
+    def SystemControl():
+        return 0
+
+    def Argvcheck():
+        print("Hello World")
+
+if not start.SystemControl():
+    start.Argvcheck()
+else:
+    start.error()
