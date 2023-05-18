eeebf470100864225155dfd0f6aa6d9625f9966d
L0elements <leoelements08@gmail.com>
2023-05-18 21:31:16 +0200

    Added the function: Create a new book

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import sys
-import pathlib
 from local.Lockpass import lockpass
 class start():
-    def error():
-        return 0
-    def SystemControl():
-        path = pathlib.PureWindowsPath("./main.py")
-        print(path)
+    def error(): #type: ignore
+        pass
+    def SystemControl(): # type: ignore
         return 0
 
-    def Argvcheck():
+    def Argvcheck(): # type: ignore
         try:
             command = sys.argv[1]
         except IndexError:
             lockpass.help()
             return 2 #noarguments
-        print(sys.argv)
         if command == 'newbook':
-            lockpass.newbook("bookone") # type: ignore
+            passwd = str(input("Write a good password for the book (DON'T FORGET IT) > "))
+            descr = str(input("Add a description (facoltative) > "))
+            if descr == "":
+                descr = "None"
+            if not lockpass.newbook(sys.argv[2], descr, passwd): # type: ignore
+                print('''Book created succesfully\n''' + "Name: " + sys.argv[2] + "\nDescription: " + descr + "\nPassword: " + passwd)
+            else:
+                print("an error has occurred: Try to relaunch the program. If the problem persists, contact the support")
+        return 0
 if not start.SystemControl():
     start.Argvcheck()
 else:
