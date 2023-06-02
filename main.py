49010c0fb70831af0a809c577fae8b83cd4133f8
L0elements <leoelements08@gmail.com>
2023-06-02 17:00:44 +0200

    Added a book view

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+"""
+Copyright (C) 2023 L0Elements
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
 # local library
 from local.Lockpass import *
 # kivy
@@ -6,10 +24,40 @@ import kivy
 kivy.require('2.2.0')
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 
 
 class root(BoxLayout):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.orientation = 'vertical'
+        self.add_widget(books())
+
+
+class books(BoxLayout):
+
+    def __init__(self):
+        super().__init__()
+        self.orientation = 'vertical'
+        b_index = lockpass.booklist()
+        if len(b_index) == 0:
+            pass
+        bookindex = BoxLayout(orientation="vertical")
+        scroll = ScrollView(do_scroll_x=False)
+        for item in b_index:
+            first_layout = BoxLayout(orientation="horizontal", height='120sp')
+            second_layout = BoxLayout(orientation="vertical", size_hint=(.8, 1))
+
+            second_layout.add_widget(Label(text=item, font_size='24sp', size_hint_y=.8, halign='left'))
+            second_layout.add_widget(Label(text=lockpass.bookinfo.Description(item), size_hint_y=.2, halign='left'))
+
+            first_layout.add_widget(second_layout)
+            first_layout.add_widget(Button(text="Open Book", size_hint=(.2, 1)))
+            bookindex.add_widget(first_layout)
+        scroll.add_widget(bookindex)
+        self.add_widget(scroll)
 
 
 class MyApp(App):
