35089f3da2ac0546a7a2ccef5afb6f8d3c28fab7
L0elements <leoelements08@gmail.com>
2023-09-12 21:05:48 +0200

    Cleaned the code Modified the methods inside the module

--- Lockpass.py
+++ Lockpass.py
@@ -1,8 +1,4 @@
 """
-TODO:
-- rewrite all the methods in order to be compatible with sqlite3
-"""
-"""
 Copyright (C) 2023 L0Elements
 
 This program is free software; you can redistribute it and/or modify
@@ -19,22 +15,17 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
-import datetime
-import time
 import sqlite3
 
 class lockpass:
     path = "Data/passwords.db"
-    db = None
-    cur = None
+    db = sqlite3.connect(path)
+    cur = db.cursor()
     def __init__(self):
-        self.db = sqlite3.connect(self.path)
         try:
-            self.cur = self.db.cursor()
             self.cur.execute("CREATE TABLE password(website, username, password)")
         except sqlite3.OperationalError:
             pass
-        
     def addpasswd(self, account_location, account_username, password): 
        #checking if the same password exists, after that creating the entry
         entry = self.cur.execute(f"SELECT * FROM password WHERE website='{account_location}' AND username='{account_username}' AND password='{password}'") 
@@ -44,36 +35,34 @@ class lockpass:
                              ''')           
             self.db.commit()
 
-    def passwdindex(bookname):
-        blist = lockpass.booklist()
-        if not bookname in blist:
-            return 2
+    def passwdindex(self):
+        return self.cur.execute("SELECT * FROM password").fetchall()
+
+    def rmpassword(self, account_location, account_username, password):
+        query = (account_location, account_username, password)
+        #finding the number of results with the correct properties (usually there has to be only one result)
+        results = self.cur.execute("SELECT COUNT(*) FROM password WHERE \
+                                   website=? AND \
+                                   username = ? AND \
+                                   password = ?", query).fetchone()
+        if results[0]:
+            self.cur.execute("DELETE FROM password WHERE\
+                                website=? AND \
+                                username = ? AND \
+                                password = ?", query)
+            self.db.commit()
         else:
-            return lp_utilities.getfilelines("Data/books/"+ bookname + "/.index")
+            raise EntryNotFoundError("Database entry Not Found")
+    
+    def changepasswd(self, old= (), new = ()):
+        if len(old) == len(new):
+            self.rmpassword(old[0], old[1], old[2])
+            self.addpasswd(new[0], new[1], new[2])          
+        else:
+            raise Exception
 
-    def rmpassword(bookname, id):
-        blist = lockpass.booklist()
-        plist = lockpass.passwdindex(bookname)
+class EntryNotFoundError(Exception):
+    pass
 
-        if bookname in blist and id in plist:
-            os.remove("Data/books/" + bookname + '/' + id)
-            nlist = []
-
-            for pass_id in plist:
-                if pass_id != id:
-                    nlist.append(pass_id)
-                else:
-                    pass
-            shutil.move("Data/books/" + bookname + "/.index", "Data/books/" + bookname + "/Recyclebin")
-            os.rename("Data/books/" + bookname + "/Recyclebin/.index", "Data/books/" + bookname + "/Recyclebin/.index" + str(time.time()) + ".old" )
-            f = open("Data/books/" + bookname + "/.index", 'w')
-            f.close()
-            del f
-            with open("Data/books/" + bookname + "/.index", 'a') as f:
-                for npass_id in nlist:
-                    if npass_id != id:
-                        f.write(npass_id + '\n')
-                    else:
-                        pass
-            return 0
-        return 2
+if __name__ == '__main__':
+   pass
